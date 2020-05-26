060113ff16726b8cd13870d45756ab63e088d201
jchan3990 <jonchan3990@gmail.com>
2020-05-26 13:23:34 -0700

    Finished react components

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 // TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover: false
+    };
+  }
+
+  onItemHover() {
+    this.setState(
+      {hover: !this.state.hover}
+    )
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+    };
+
+    return (
+      <div style={style} onMouseOver={this.onItemHover.bind(this)}>{this.props.item}</div>
+    )
+  }
+}
+
+var GroceryList = (props) => (
+  <div>
+      <h3> My Grocery List </h3>
+      {props.items.map(item =>
+        <GroceryListItem item={item} />
+      )}
+  </div>
+);
+
+ReactDOM.render(<GroceryList items={['Honey Bunches of Oats', 'Whole Milk']}/>, document.getElementById('app'));
